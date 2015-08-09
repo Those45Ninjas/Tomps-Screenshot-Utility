0e5cd072774b36151e7e874e47af2130e3235b39
Those45Ninjas <tom.parker1235@gmail.com>
2015-08-09 22:11:05 +0800

    Created Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace tomps_screenshot_utility
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveCaptureDialog = new System.Windows.Forms.SaveFileDialog();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.captureToFileButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "All Displays",
+            "Window",
+            "Region"});
+            this.comboBox1.Location = new System.Drawing.Point(4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(312, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Capture Mode";
+            // 
+            // captureToFileButton
+            // 
+            this.captureToFileButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.captureToFileButton.Location = new System.Drawing.Point(0, 0);
+            this.captureToFileButton.Name = "captureToFileButton";
+            this.captureToFileButton.Size = new System.Drawing.Size(154, 53);
+            this.captureToFileButton.TabIndex = 1;
+            this.captureToFileButton.Text = "Capture and save to a file";
+            this.captureToFileButton.UseVisualStyleBackColor = true;
+            this.captureToFileButton.Click += new System.EventHandler(this.captureToFileButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Capture and upload to imgur.com";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(4, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.captureToFileButton);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(312, 53);
+            this.splitContainer1.SplitterDistance = 154;
+            this.splitContainer1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 82);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.comboBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(4);
+            this.Text = "Tomps Screenshot Utility Utility 0.1.0b";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SaveFileDialog saveCaptureDialog;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button captureToFileButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace tomps_screenshot_utility
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<string, Rectangle> screens;
+        public Form1()
+        {
+            InitializeComponent();
+
+            screens = new Dictionary<string, Rectangle>();
+            int i = 0;
+            foreach(Screen screen in Screen.AllScreens)
+            {
+                string name; 
+                if(screen == Screen.PrimaryScreen)
+                    name = String.Format("Display {0} (Primary)", i);
+                else
+                   name = String.Format("Display {0}",i);
+
+                comboBox1.Items.Add(name);
+                screens.Add(name, screen.Bounds);
+                i++;
+            }
+        }
+        public Bitmap CaptureReigon(Rectangle rect)
+        {
+            Bitmap bitmap = new Bitmap(rect.Width, rect.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.CopyFromScreen(rect.X, rect.Y, 0, 0, new Size(rect.Width, rect.Height));
+            }
+            return bitmap;
+        }
+        private void CaptureToFile()
+        {
+            this.WindowState = FormWindowState.Minimized;
+
+            // Capture the image
+            Bitmap bitmap;
+
+            string selectedValue = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(selectedValue + " is " + screens.ContainsKey(selectedValue).ToString());
+            if (screens.ContainsKey(selectedValue))
+                bitmap = CaptureReigon(screens[selectedValue]);
+            else if (selectedValue == "Window")
+                bitmap = CaptureReigon(SystemInformation.VirtualScreen);
+            else if (selectedValue == "Region")
+                bitmap = CaptureReigon(SystemInformation.VirtualScreen);
+            else
+                bitmap = CaptureReigon(SystemInformation.VirtualScreen);
+
+            // Save or upload the image
+            if (saveCaptureDialog.ShowDialog() == DialogResult.OK)
+                bitmap.Save(saveCaptureDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+
+            this.WindowState = FormWindowState.Normal;
+        }
+        private void captureToFileButton_Click(object sender, EventArgs e)
+        {
+            CaptureToFile();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CaptureToImgur();
+        }
+
+        private void CaptureToImgur()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
